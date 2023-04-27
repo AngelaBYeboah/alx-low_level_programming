5d7af9ecf4a90d2ec70743a1fbc1d046561c1a03
AngelaBYeboah <abyeboah18@gmail.com>
2023-04-26 23:00:12 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -19,12 +19,12 @@ int main(void)
 		{
 			if (p != d)
 			{
-				putchar(d);
-				putchar(p);
-				if (d == '8' && p == '9')
-				continue;
-				putchar(',');
-				putchar(' ');
+			putchar(d);
+			putchar(p);
+			if (d == '8' && p == '9')
+			continue;
+			putchar(',');
+			putchar(' ');
 			}
 		}
 	}
