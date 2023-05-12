66a2b75ee7d4ba010269107878e0cb48f7f23ffd
AngelaBYeboah <abyeboah18@gmail.com>
2023-05-11 21:42:38 -0700

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,14 +8,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s == '\0')
+	if (*s == '\0')
 	{
-		_put char('\n');
+		_putchar('\n');
 		return;
 	}
 	_putchar(*s);
 	s++;
 	_puts_recursion(s);
-}
-
-		
+}		
